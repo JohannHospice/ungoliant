1ed3461079eff34fb3d9cab9ccec8dbc2ee2120b
JohannHospice <johannhospice.dev@gmail.com>
2017-02-16 10:39:14 +0100

    Update scripts
    Fix library installation
    Only use libcurl and tidy
    Change installation type(only put them in lib/ dir)

--- script/install-cmake.sh
+++
@@ -1,9 +0,0 @@
-mkdir tmp
-cd tmp
-curl https://cmake.org/files/v3.8/cmake-3.8.0-rc1.tar.gz > cmake-3.8.0-rc1.tar.gz
-tar zxvf cmake-3.8.0-rc1.tar.gz
-cd cmake-3.8.0-rc1
-./configure
-make
-sudo make install
-cd ../../ && rm -rf tmp
--- script/install-tidy.sh
+++ script/install-tidy.sh
@@ -1,9 +1,5 @@
-mkdir tmp
-cd tmp
+mkdir ../lib
+cd ../lib 
 wget https://github.com/htacg/tidy-html5/archive/master.zip
 unzip master.zip
-cd master/build/cmake
-cmake ../.. -DCMAKE_BUILD_TYPE=Release [-DCMAKE_INSTALL_PREFIX=/path/for/install]
-make
-sudo make install
-cd ../../ && rm -rf tmp
+rm master.zip
