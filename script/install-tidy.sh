14a6be7b5dbba96da1c23d5025f30f22da105ce7
JohannHospice <johannhospice.dev@gmail.com>
2017-02-16 18:43:38 +0100

    Remove scripts

--- script/install-tidy.sh
+++
@@ -1,5 +0,0 @@
-mkdir ../lib
-cd ../lib 
-wget https://github.com/htacg/tidy-html5/archive/master.zip
-unzip master.zip
-rm master.zip
--- script/install.sh
+++
@@ -1,4 +0,0 @@
-chmod 777 install-tidy.sh 
-chmod 777 install-libcurl.sh
-./install-tidy.sh
-./install-libcurl.sh
