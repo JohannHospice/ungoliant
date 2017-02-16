e293d13ceb7bdbe9e80ecb45478bca0e6a94957b
JohannHospice <johannhospice.dev@gmail.com>
2017-02-16 10:39:14 +0100

    Update scripts
    Fix library installation
    Only use libcurl and tidy
    Change installation type(only put them in lib/ dir)

--- script/install-libcurl.sh
+++ script/install-libcurl.sh
@@ -1,9 +1,5 @@
-mkdir tmp
-cd tmp
-curl https://curl.haxx.se/download/curl-7.52.1.tar.gz > curl-7.52.1.tar.gz
+mkdir ../lib
+cd ../lib 
+wget https://curl.haxx.se/download/curl-7.52.1.tar.gz
 tar zxvf curl-7.52.1.tar.gz
-cd curl-7.52.1
-./configure
-make
-sudo make install 
-cd ../../ && rm -rf tmp
+rm curl-7.52.1.tar.gz
--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,4 @@
-chmod 777 install-cmake.sh 
-chmod 777 install-libcurl.sh 
 chmod 777 install-tidy.sh 
-./install-cmake.sh
+chmod 777 install-libcurl.sh
 ./install-tidy.sh
 ./install-libcurl.sh
