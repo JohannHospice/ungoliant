6031ece962da74e4eb7950eb4091db8ff2de1ab8
JohannHospice <johannhospice.dev@gmail.com>
2017-02-16 10:50:45 +0100

    Adding header to implement and link into ungoliant

--- src/queryengine/queryengine.c
+++
@@ -1,1 +0,0 @@
-queryengine.c
--- src/ungoliant.c
+++ src/ungoliant.c
@@ -1,3 +1,8 @@
-int main(int argc, char const *argv[]){
+#include "parser/parser.h"
+#include "crawler/crawler.h"
+#include "indexer/indexer.h"
+#include "queryengine/queryengine.h"
+
+int main(int argc, char const *argv[]) {
 	return 0;
 }
