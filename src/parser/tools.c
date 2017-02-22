a3b1dabcb31aee6a7fe871bc799442efbf3057fe
JohannHospice <johannhospice.dev@gmail.com>
2017-02-22 13:30:02 +0100

    Add tools for parser

---
+++ src/parser/tools.c
@@ -0,0 +1,7 @@
+/* curl write callback, to fill tidy's input buffer...  */ 
+uint write_cb(char *in, uint size, uint nmemb, TidyBuffer *out){
+  uint r;
+  r = size * nmemb;
+  tidyBufAppend(out, in, r);
+  return r;
+}
