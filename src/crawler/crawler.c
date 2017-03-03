375f43895bcdd4c7a48bc6b9bfab14be6524b7fa
JohannHospice <johannhospice.dev@gmail.com>
2017-03-03 14:20:55 +0100

    Erreur seg. presque resolue

--- src/crawler/crawler.c
+++ src/crawler/crawler.c
@@ -34,30 +34,39 @@ void crawl(char *url) {
     tidyBufInit(&docbuf);
 
     char curl_errbuf[CURL_ERROR_SIZE];
-    CURL *curl = initialiseCURL(url, curl_errbuf, docbuf);
-    if(curl && !curl_easy_perform(curl)) {
-        if (parse(tdoc, docbuf) >= 0) {
-            printf("\nparsed:\n");
-            tidySaveStdout(tdoc);
-            TidyNode *nodes = malloc(sizeof(TidyNode));
-            int nodes_size = queryNodeByDoc(nodes, 0, tdoc, tidyGetRoot(tdoc), "a");
+      CURL *curl = curl_easy_init();
+      if(curl) {
+          curl_easy_setopt(curl, CURLOPT_URL, url);
+          curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_errbuf);
+          curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+          curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+          curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+          curl_easy_setopt(curl, CURLOPT_WRITEDATA, &docbuf);
+          if(!curl_easy_perform(curl)) {
+              if (parse(tdoc, docbuf) >= 0) {
+                  printf("\nparsed:\n");
+                  tidySaveStdout(tdoc);
+                  TidyNode *nodes = malloc(sizeof(TidyNode)*100);
+                  int nodes_size = queryNodeByDoc(nodes, 0, tdoc, tidyGetRoot(tdoc), "a");
 
-            char *attrs[nodes_size];
-            queryAttrByAllNodes(attrs, nodes, nodes_size, "href");
+                  char *attrs[nodes_size];
+                  queryAttrByAllNodes(attrs, nodes, nodes_size, "href");
 
-            printf("\nextract %d a[href=]:\n", nodes_size);
-            printArrayString(attrs, nodes_size);
+                  printf("\nextract %d a[href=]:\n", nodes_size);
+                  printArrayString(attrs, nodes_size);
 
-            free(nodes);
-            tidyBufFree(&docbuf);
-            tidyBufFree(&tidy_errbuf);
-            tidyRelease(tdoc);
-        }
-        else
-            fprintf(stderr, "%s\n", tidy_errbuf.bp);
-    } else {
-        perror("error");
-        fprintf(stderr, "%s\n", curl_errbuf);
-    }
+                  free(nodes);
+                  tidyBufFree(&docbuf);
+                  tidyBufFree(&tidy_errbuf);
+                  tidyRelease(tdoc);
+              }
+              else
+                  fprintf(stderr, "%s\n", tidy_errbuf.bp);
+          } else {
+              perror("error");
+              fprintf(stderr, "%s\n", curl_errbuf);
+          }
+      }
+      printf("# end\n");
     curl_easy_cleanup(curl);
 }
