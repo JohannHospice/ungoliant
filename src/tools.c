beb4d0aeb1df6c6631289f9ea1aa379a87734667
JohannHospice <johannhospice.dev@gmail.com>
2017-03-03 12:43:15 +0100

    Adding cleanning array function

--- src/tools.c
+++ src/tools.c
@@ -4,3 +4,15 @@ void printArrayString(char **in , int in_size) {
     for (int i = 0; i < in_size; ++i)
         printf("%s\n", in [i]);
 }
+
+void cleanArray(char **buffer, int size){
+    for(int i = 0; i < size; i++){
+        if(buffer[i] == NULL){
+            int j = i;
+            while(j< size - 1){
+              buffer[j] = buffer[j+1];
+            }
+            buffer[j+1] = NULL;
+        }
+    }
+}
