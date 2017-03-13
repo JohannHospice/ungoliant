6063863482d1352092a64c0ac1803b57a0aff1f6
JohannHospice <johannhospice.dev@gmail.com>
2017-03-13 17:16:25 +0100

    Ajout du crawl recursif

--- src/crawler/crawler.c
+++ src/crawler/crawler.c
@@ -1,84 +1,97 @@
-#include <curl.h> 
-#include <tidy.h> 
-#include <tidybuffio.h> 
-#include "../parser/parser.h"
+#include <curl.h>
+#include <tidy.h>
+#include <string.h>
+#include <tidybuffio.h>
+#include "crawler.h"
 #include "../tools.h"
+#include "../parser/parser.h"
 
 uint write_cb(char *in, uint size, uint nmemb, TidyBuffer *out) {
-  uint r = size * nmemb;
-  tidyBufAppend(out, in, r);
-  return r;
+    uint r = size * nmemb;
+    tidyBufAppend(out, in, r);
+    return r;
 }
 
-void crawlUrl(char *url) {
-  TidyBuffer docbuf = {0};
-  TidyBuffer tidy_errbuf = {0};
-  TidyDoc tdoc = tidyCreate();
-  tidyOptSetBool(tdoc, TidyForceOutput, yes);
-  tidyOptSetInt(tdoc, TidyWrapLen, 4096);
-  tidySetErrorBuffer(tdoc, &tidy_errbuf);
-  tidyBufInit( &docbuf);
-
-  char curl_errbuf[CURL_ERROR_SIZE];
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_errbuf);
-    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &docbuf);
-    if (!curl_easy_perform(curl)) {
-      if (parseBuffer(tdoc, docbuf) >= 0) {
-        TidyNode *nodes = malloc(sizeof(TidyNode) *100);
-        int nodes_size = queryNodeByDoc(nodes, 0, tdoc, tidyGetRoot(tdoc), "a");
-
-        char *attrs[nodes_size];
-        queryAttrByAllNodes(attrs, nodes, nodes_size, "href");
-
-        printf("\nextract %d a[href=]:\n", nodes_size);
-        printArrayString(attrs, nodes_size);
-
-        free(nodes);
-        tidyBufFree( &docbuf);
-        tidyBufFree( &tidy_errbuf);
-        tidyRelease(tdoc);
-      } else
-        fprintf(stderr, "%s\n", tidy_errbuf.bp);
-    } else {
-      perror("error");
-      fprintf(stderr, "%s\n", curl_errbuf);
+void crawlUrlRec(char *url, int rec) {
+    if(rec >= 0) {
+        printf("rec %d\n", rec);
+        struct info_page *info_page = crawlUrl(url);
+        if(info_page) {
+            info_page->info_pages = calloc(info_page->links_size, sizeof(struct info_page*));
+            printInfoPage(info_page);
+            for (int i = 0; i < info_page->links_size; ++i)
+                crawlUrlRec(info_page->links[i], rec - 1);
+            freeInfoPage(&info_page);
+        }
+		printf("\n");
     }
-  }
-  printf("# end\n");
-  curl_easy_cleanup(curl);
 }
 
-//ne fonctionne pas encore
-void crawlFile(char *filename) {
-  TidyBuffer docbuf = {0};
-  TidyBuffer tidy_errbuf = {0};
-  TidyDoc tdoc = tidyCreate();
-  tidyOptSetBool(tdoc, TidyForceOutput, yes);
-  tidyOptSetInt(tdoc, TidyWrapLen, 4096);
-  tidySetErrorBuffer(tdoc, &tidy_errbuf);
-  tidyBufInit( &docbuf);
+struct info_page *crawlUrl(char *url) {
+    struct info_page *info_page = NULL;
 
-  if (parseFile(tdoc, filename) >= 0) {
-    TidyNode *nodes = malloc(sizeof(TidyNode) *100);
-    int nodes_size = queryNodeByDoc(nodes, 0, tdoc, tidyGetRoot(tdoc), "a");
+    TidyBuffer docbuf = {0};
+    TidyBuffer tidy_errbuf = {0};
+    TidyDoc tdoc = tidyCreate();
+    tidyOptSetBool(tdoc, TidyForceOutput, yes);
+    tidyOptSetInt(tdoc, TidyWrapLen, 4096);
+    tidySetErrorBuffer(tdoc, &tidy_errbuf);
+    tidyBufInit( &docbuf);
 
-    char *attrs[nodes_size];
-    queryAttrByAllNodes(attrs, nodes, nodes_size, "href");
+    char curl_errbuf[CURL_ERROR_SIZE];
+    CURL *curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+        curl_easy_setopt(curl, CURLOPT_POSTREDIR, 3);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &docbuf);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_errbuf);
+        if (!curl_easy_perform(curl)) {
+            char *tmp;
+            info_page = malloc(sizeof(struct info_page));
+            if(curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &tmp) == 0) {
+                int tmplen = strlen(tmp);
+                info_page->url = calloc(tmplen, sizeof(char));
+                for (int i = 0; i < tmplen; ++i)
+                    info_page->url[i] = tmp[i];
+            }
+            if (parseBuffer(tdoc, docbuf) >= 0) {
+                TidyNode *nodes;
+                int nodes_size;
+                queryNodeByDoc(&nodes, &nodes_size, tdoc, tidyGetRoot(tdoc), "a");
 
-    printf("\nextract %d a[href=]:\n", nodes_size);
-    printArrayString(attrs, nodes_size);
+                queryAttrByAllNodes(&(info_page->links), nodes, nodes_size, "href");
+                info_page->links_size = nodes_size;
 
-    free(nodes);
-    tidyBufFree( &docbuf);
-    tidyBufFree( &tidy_errbuf);
-    tidyRelease(tdoc);
-  } else
-    fprintf(stderr, "%s\n", tidy_errbuf.bp);
-  printf("# end\n");
+                free(nodes);
+                tidyBufFree(&docbuf);
+                tidyBufFree(&tidy_errbuf);
+                tidyRelease(tdoc);
+            } else {
+                fprintf(stderr, "url: %s\nerror tidy: %s\n", url, tidy_errbuf.bp);
+                info_page = NULL;
+            }
+        } else {
+            fprintf(stderr, "url: %s\nerror curl: %s\n", url, curl_errbuf);
+            info_page = NULL;
+        }
+        curl_easy_cleanup(curl);
+    }
+    return info_page;
+}
+
+void printInfoPage(struct info_page *info_page) {
+    printf("url: %s\nlinks_size: %d\nlinks:\n", info_page->url, info_page->links_size);
+    printArrayString(info_page->links, info_page->links_size);
+}
+
+void freeInfoPage(struct info_page **info_page) {
+    for (int i = 0; i < (*info_page)->links_size; ++i)
+        free((*info_page)->links[i]);
+    free((*info_page)->url);
+    free(*info_page);
+    *info_page = NULL;
 }
--- src/crawler/crawler.h
+++ src/crawler/crawler.h
@@ -1,2 +1,13 @@
-void crawlUrl(char *url);
+//#include "../tools.h"
+struct info_page{
+	char *url;
+	char **links;
+	int links_size;
+	struct info_page **info_pages;
+};
+
+void crawlUrlRec(char *url, int rec);
+void printInfoPage(struct info_page *info_page);
+struct info_page *crawlUrl(char *url);
 void crawlFile(char *filename);
+void freeInfoPage(struct info_page **info_page);
