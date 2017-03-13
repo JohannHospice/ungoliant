8a06ab7d79e9e9c7b6883fa1de6e54478a2a7463
JohannHospice <johannhospice.dev@gmail.com>
2017-03-13 17:15:13 +0100

    Changement des paramatres de certainnes fonction pour un passage par adresse

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,8 +1,8 @@
 #include <curl.h>
 #include <tidy.h>
 
-int queryNodeByDoc(TidyNode *buffer, int size, TidyDoc doc, TidyNode tnod, char *target);
-char *queryAttrByNode(TidyNode node, char *target);
-char **queryAttrByAllNodes(char **buffers, TidyNode *nodes, int nodes_size, char *target);
+void queryNodeByDoc(TidyNode **buffer, int *size, TidyDoc doc, TidyNode tnod, char *target);
 int parseBuffer(TidyDoc tdoc, TidyBuffer docbuf);
 int parseFile(TidyDoc tdoc, char *filename);
+void queryAttrByNode(char **buffer, TidyNode node, char *target);
+void queryAttrByAllNodes(char ***buffers, TidyNode *nodes, int nodes_size, char *target);
