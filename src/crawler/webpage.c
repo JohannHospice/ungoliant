c8962adafda03caa58886fcb0f3107c1a91e0c43
JohannHospice <johannhospice.dev@gmail.com>
2017-03-15 18:24:43 +0100

    Fin du crawl en recursif
    creation de structures contenant les information sur les page web

---
+++ src/crawler/webpage.c
@@ -0,0 +1,39 @@
+#include "webpage.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../tools.h"
+
+int getTotalSize(struct treepage *treepage) {
+    int sum = 1;
+    for (int i = 0; i < treepage->treepage_size; ++i){
+        sum += getTotalSize(treepage->treepage[i]);
+    }
+    return sum;
+}
+void printInfoPage(struct infopage *infopage) {
+    printf("url: %s\nlinks_size: %d\nlinks:\n", infopage->url, infopage->links_size);
+    printArrayString(infopage->links, infopage->links_size);
+}
+
+void printTreepage(struct treepage *treepage) {
+    printf("depth: %d\ntreepage_size: %d\n", treepage->depth, treepage->treepage_size);
+    printInfoPage(treepage->infopage);
+    for (int i = 0; i < treepage->treepage_size; ++i)
+        printTreepage(treepage->treepage[i]);
+}
+
+void freeInfoPage(struct infopage **infopage) {
+    for (int i = 0; i < (*infopage)->links_size; ++i)
+        free((*infopage)->links[i]);
+    free((*infopage)->url);
+    free(*infopage);
+    *infopage = NULL;
+}
+
+void freeTreepage(struct treepage **treepage) {
+    for (int i = 0; i < (*treepage)->treepage_size; ++i)
+        freeTreepage(&(*treepage)->treepage[i]);
+    freeInfoPage(&(*treepage)->infopage);
+    free(*treepage);
+    *treepage = NULL;
+}
--- test/crawler-test.c
+++ test/crawler-test.c
@@ -1,11 +1,23 @@
-#include "../src/crawler/crawler.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../src/crawler/crawler.h"
+#include "../src/crawler/webpage.h"
+
 int main(int argc, char **argv) {
-	printf("\n\tCRAWLER TEST on %s\n\n", argv[1]);
-    if(argc < 2){
-    	printf("Need argument url\n");
-	    return 0;
+    char *url = (argc > 1) ? argv[1] : "https://github.com/";
+    int depth = (argc > 2) ? atoi(argv[2]) : 2;
+    
+	printf("\n\tCRAWLER TEST on %s with %d of depth\n\n", url, depth);
+    printf("relative link not supported\n");
+    printf("crawling (1=success, 0=failure, .=crawl/parse)...\n");
+    struct treepage *treepage = crawlUrlRec(url, depth, depth);
+    if(treepage){
+	    printf("\nnumber of pages crawled: %d\n\n\tDISPLAY tree\n\n", getTotalSize(treepage));
+		printTreepage(treepage);
+	    freeTreepage(&treepage);
     }
-	crawlUrlRec(argv[1], 2);
+    else
+    	printf("no result\n");
     return 0;
 }
