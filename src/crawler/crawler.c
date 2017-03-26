182bfc71b71b6575376c8a80f933f37fa5ae93e0
Johann Hospice <johannhospice.dev@gmail.com>
2017-03-26 01:56:22 +0100

    Refactor crawler and tools

--- src/crawler/crawler.c
+++ src/crawler/crawler.c
@@ -1,8 +1,11 @@
 #include <curl/curl.h>
 #include <tidy.h>
 #include <tidybuffio.h>
+#include <string.h>
+#include "../tools.h"
 #include "../parser/parser.h"
 #include "crawler.h"
+#include "parseurl.h"
 #include "webpage.h"
 
 uint write_cb(char *in, uint size, uint nmemb, TidyBuffer *out) {
@@ -11,62 +14,6 @@ uint write_cb(char *in, uint size, uint nmemb, TidyBuffer *out) {
     return r;
 }
 
-char *dynaclone(char *src){
-    int len = strlen(src);
-    char *dest = calloc(len, sizeof(char));
-    for (int i = 0; i < len; ++i)
-        dest[i] = src[i];
-    return dest;
-}
-
-char *dynaAppend(char *src1, int len1, char *src2, int len2){
-    char *dest = calloc(len1 + len2, sizeof(char));
-    for (int i = 0; i < len1; ++i)
-        dest[i] = src1[i];
-    for (int i = len1; i < len1 + len2; ++i)
-        dest[i] = src2[i];
-    return dest;
-}
-
-void append(char **src1, int len1, char *src2, int len2){
-    int sizeof_src1 = sizeof(*src1),
-        sizeof_src2 = sizeof(src2);
-
-    if(sizeof_src1 < sizeof_src1 + sizeof_src2)
-        *src1 = realloc(*src1, sizeof_src1 + sizeof_src2);
-
-    for (int i = len1; i < len1 + len2; ++i)
-        *src1[i] = src2[i];
-
-}
-
-
-void normalizeUrl(char **url, int url_size, char *host, int host_size){
-    if(*url[0] == '/'){
-        char *tmp = dynaAppend(host, host_size, *url, url_size);
-        free(url);
-        *url = tmp;
-    }
-}
-/*
-regex absolute url
-^
-((\w{3,9}?):\/\/)?
-((?:(\w+):(\w+)@)?([A-Za-z0-9.\-]+\.\w+)(?::(\d+))?)
-(([A-Za-z0-9.\/\-%]+)+)?
-(?:\?([a-zA-Z0-9_-]+=[a-zA-Z0-9_\-\{\}\[\],;]*|\&)+)?
-(?:#[a-zA-Z0-9_-]*)?
-$
-*/
-void normalizeUrls(char **urls, int urls_size, char *host, int host_size){
-    for (int i = 0; i < urls_size; ++i) {
-        normalizeUrl(&(urls[i]), strlen(urls[i]), host, host_size);
-    }
-}
-
-/**
- * TODO: save info pages
- */
 struct treepage *crawlUrlRec(char *url, int max, int depth) {
     if(depth <= 0)
         return NULL;
@@ -146,15 +93,8 @@ struct infopage *crawlUrl(char *url) {
                 tidyBufFree(&docbuf);
                 tidyBufFree(&tidy_errbuf);
                 tidyRelease(tdoc);
-            } /*
-            else 
-                fprintf(stderr, "url: %s\nerror tidy: %s\n", url, tidy_errbuf.bp);
-            */
-        } 
-        /*
-        else 
-            fprintf(stderr, "url: %s\nerror curl: %s\n", url, curl_errbuf);
-        */
+            }
+        }
         curl_easy_cleanup(curl);
     }
     return infopage;
